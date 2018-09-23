f122c28cda571896c34e1760e5e363897f4e4a36
bartbutenaers <bart.butenaers@gmail.com>
2018-09-23 22:55:23 +0200

    indentations

--- audio_analyser.js
+++ audio_analyser.js
@@ -16,63 +16,63 @@
 module.exports = function(RED) {
 	var AudioAnalyser = require('audio-analyser');
 
-	function WavHeadersNode(config) {
+	function AudioAnalyserNode(config) {
 		RED.nodes.createNode(this, config);
-    this.minDecibels           = config.minDecibels || -100;
-    this.maxDecibels           = config.maxDecibels || -30;
-    this.fftSize               = config.fftSize || 1024;
-    this.smoothingTimeConstant = config.smoothingTimeConstant || 0.2;
-    this.channel               = config.channel || 0;
-    this.bufferSize            = config.bufferSize || 44100;
+        this.minDecibels           = config.minDecibels || -100;
+        this.maxDecibels           = config.maxDecibels || -30;
+        this.fftSize               = config.fftSize || 1024;
+        this.smoothingTimeConstant = config.smoothingTimeConstant || 0.2;
+        this.channel               = config.channel || 0;
+        this.bufferSize            = config.bufferSize || 44100;
 
-    this.audioAnalyser = new AudioAnalyser({
-        // Magnitude diapasone, in dB
-        minDecibels: config.minDecibels,
-        maxDecibels: config.maxDecibels,
-        // Number of time samples to transform to frequency
-        fftSize: config.fftSize,
-        // Number of frequencies, twice less than fftSize
-        frequencyBinCount: config.fftSize/2,
-        // Smoothing, or the priority of the old data over the new data
-        smoothingTimeConstant: config.smoothingTimeConstant,
-        // Number of channel to analyse
-        channel: config.channel,
-        // Size of time data to buffer
-        bufferSize: config.bufferSize,
-        // Windowing function for fft, https://github.com/scijs/window-functions
-        applyWindow: function (sampleNumber, totalSamples) {
-        }
-        //...pcm-stream params, if required
-    })
-    
-    audioAnalyser.on('data', function (chunk) {
-	    var floatFreq = this.getFloatFrequencyData(new Float32Array(this.fftSize));
-	    var floatTime = this.getFloatTimeDomainData(new Float32Array(this.fftSize));
-	    var byteFreq = this.getByteFrequencyData(new Uint8Array(this.fftSize));
-	    var byteTime = this.getByteTimeDomainData(new Uint8Array(this.fftSize));
-	    var freq = this.getFrequencyData();
-	    var time = this.getTimeData();
-
-      // TODO node.send( ... );
-    });
-
-    var node = this;
-
-    node.on("input", function(msg) {
-      var audioChunk = msg.payload;
-      
-        if (!audioChunk) {
-          audioChunk = new Buffer();
-        }
+        var node = this;
         
-        if (!Buffer.isBuffer(audioChunk)) {
-          return;
-        }
-    
-        // This will trigger the on-data call of the audio analyser
-        audioAnalyser._transform(audioChunk);
-    });
-  }
+        node.audioAnalyser = new AudioAnalyser({
+            // Magnitude diapasone, in dB
+            minDecibels: config.minDecibels,
+            maxDecibels: config.maxDecibels,
+            // Number of time samples to transform to frequency
+            fftSize: config.fftSize,
+            // Number of frequencies, twice less than fftSize
+            frequencyBinCount: config.fftSize/2,
+            // Smoothing, or the priority of the old data over the new data
+            smoothingTimeConstant: config.smoothingTimeConstant,
+            // Number of channel to analyse
+            channel: config.channel,
+            // Size of time data to buffer
+            bufferSize: config.bufferSize,
+            // Windowing function for fft, https://github.com/scijs/window-functions
+            applyWindow: function (sampleNumber, totalSamples) {
+            }
+            //...pcm-stream params, if required
+        })
+        
+        node.audioAnalyser.on('data', function (chunk) {
+            var floatFreq = this.getFloatFrequencyData(new Float32Array(this.fftSize));
+            var floatTime = this.getFloatTimeDomainData(new Float32Array(this.fftSize));
+            var byteFreq = this.getByteFrequencyData(new Uint8Array(this.fftSize));
+            var byteTime = this.getByteTimeDomainData(new Uint8Array(this.fftSize));
+            var freq = this.getFrequencyData();
+            var time = this.getTimeData();
+
+          // TODO node.send( ... );
+        });
+
+        node.on("input", function(msg) {
+            var audioChunk = msg.payload;
+          
+            if (!audioChunk) {
+              audioChunk = new Buffer();
+            }
+            
+            if (!Buffer.isBuffer(audioChunk)) {
+              return;
+            }
+        
+            // This will trigger the on-data call of the audio analyser
+            node.audioAnalyser._transform(audioChunk);
+        });
+    }
   
-	RED.nodes.registerType("wav-headers", WavHeadersNode);
+	RED.nodes.registerType("audio-analyser", AudioAnalyserNode);
 }
